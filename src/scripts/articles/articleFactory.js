80bda13903494b6559ff1b7ce3636a09761f0d89
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-06-29 14:30:43 -0500

    changed articlefactory

--- src/scripts/articles/articleFactory.js
+++ src/scripts/articles/articleFactory.js
@@ -1,5 +1,6 @@
 //This module was created by Brendan Abernethy
 
+//this function creates a new article entry
 //userId, title, description, url, date
 const newArticle = (userId, title, description, url, date) => ({
     userId: userId,
