4ac97ff26453935261b96dc4753d60b1c0f979ca
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-06-29 14:00:17 -0500

    created article factory

---
+++ src/scripts/articles/articleFactory.js
@@ -0,0 +1,12 @@
+//This module was created by Brendan Abernethy
+
+//userId, title, description, url, date
+const newArticle = (userId, title, description, url, date) => ({
+    userId: userId,
+    title: title,
+    description: description,
+    url: url,
+    date: date
+})
+
+export default newArticle
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 import { API, dbResponseData } from "./databaseCalls.js"
+import newArticle from "./articles/articleFactory.js"
 
 console.log('Hello World')
 
@@ -6,7 +7,7 @@ console.log('Hello World')
 //parameters for API.getData are (url, table, expand)
 //to use this, input url, the table you are looking for, and the table you wish to include (probably user)
 //example: ("http://localhost:3000", "tasks", "user")
-API.getData("http://localhost:3000", "tasks", "user").then(
+API.getData("http://localhost:3000", "articles", "user").then(
     () => {
         console.log("hello?"),
             dbResponseData.forEach(
@@ -17,3 +18,11 @@ API.getData("http://localhost:3000", "tasks", "user").then(
             )
     }
 )
+
+
+//Brendan here testing article factory and post method
+//userId, title, description, url, date
+let testArticle = newArticle(2, "articleTest", "a test of article functionality", "https://www.theonion.com/study-finds-gap-widening-between-rich-pets-and-poor-ame-1844134668", "2020-06-29")
+
+//url, table, newObject
+API.postData("http://localhost:3000", "articles", testArticle)
