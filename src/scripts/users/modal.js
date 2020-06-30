220745b9d9dce38cdc3a6cbca0516914b92870ae
Rob Mixon <mixonroberth@gmail.com>
2020-06-30 18:28:09 -0500

    sign in and register buttons working

--- src/scripts/users/modal.js
+++ src/scripts/users/modal.js
@@ -28,6 +28,7 @@ const modalFunction = () => {
     const modal = document.getElementById('regModal');
     const modalBtn = document.getElementById('registrationButton')
     const closeBtn = document.querySelector(".modal__close_bar");
+    const registerBtn = document.querySelector("#register")
 
     modalBtn.onclick = function() {
     modal.style.display = "block";
@@ -35,11 +36,17 @@ const modalFunction = () => {
     closeBtn.onclick = function() {
     modal.style.display = "none";
     }
+    registerBtn.onclick = function() {
+    modal.style.display = "none";
+    }
     window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
         }
     }
+
 }
+
+
 
 export default modalFunction
