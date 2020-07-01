fe888e85203818dba5991283d7c3f45f2a75ac2c
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-07-01 14:43:33 -0500

    cleaned up files

--- src/scripts/events/eventDOM.js
+++ src/scripts/events/eventDOM.js
@@ -1,35 +1,3 @@
-
-
-// const eventConverter = {
-//     eventEntry (obj) {
-//         const eventHTMLRepresentation = 
-//         `
-//         <div id="event__${obj.id}" class="singleEvent">
-//             <div class="event__header">
-//                 <div class="event__name">
-//                 <h4>${obj.title}</h4>
-//                     <button id="deleteEventBtn__${obj.id}" class="deleteBtn" type="button">&times;</button>
-//                 </div>    
-//             </div>
-//         <div class="event__details">
-//             <dl>
-//                 <dt>HOST</dt>
-//                     <dd>${obj.user.userId}</dd>
-//                 <dt>WHEN</dt>
-//                     <dd>${obj.date}</dd>
-//                 <dt>WHERE</dt>
-//                     <dd>${obj.location}</dd>
-//             </dl>
-//             </div>
-//         </div>
-//         `
-//         return eventHTMLRepresentation
-//     }
-// }
-
-// export default eventConverter;
-
-
 //This module was created by Brendan Abernethy
 import { API, dbResponseData } from "../databaseCalls.js"
 
