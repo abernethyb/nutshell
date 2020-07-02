f8d3ef455777e6823a4c3d806fc9752afece84d1
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-07-01 22:44:54 -0500

    made friendFactory

---
+++ src/scripts/friends/friendFactory.js
@@ -0,0 +1,8 @@
+//This module was created by Brenda Abernethy
+//userId, title, location, date
+const newFriend = (userId, activeUserId) => ({
+    userId: userId,
+    activeUserId: activeUserId
+})
+
+export default newFriend
