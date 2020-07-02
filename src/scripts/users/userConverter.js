01580c142fff9ed647ac78628964314086f6b6f4
Chad Jones <chadjones839@gmail.com>
2020-07-01 22:19:14 -0500

    final CSS uploaded

--- src/scripts/users/userConverter.js
+++ src/scripts/users/userConverter.js
@@ -2,7 +2,7 @@ const userConverter = (obj) => {
     const userHTML = 
     `
     <div class="user__profilePicture">
-            <img class="mainUserImage" src="./images/userIcon.png">
+            <img class="mainUserImage" src="${obj.image}">
         </div>
         <div class="user__userName">
         <p>${obj.username} <sup>&#8226;</sup></p>
